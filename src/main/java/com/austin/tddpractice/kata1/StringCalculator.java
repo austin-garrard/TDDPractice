d80dd9503e5e23b95a1825089a48178303a72eb5
Austin Garrard <wootasaurus@gmail.com>
2016-01-02 12:54:39 -0600

    StringCalculator sums zero arguments

--- src/main/java/com/austin/tddpractice/kata1/StringCalculator.java
+++ src/main/java/com/austin/tddpractice/kata1/StringCalculator.java
@@ -2,4 +2,8 @@ package com.austin.tddpractice.kata1;
 
 
 public class StringCalculator {
+
+    public int Add(String numbers) {
+        return 0;
+    }
 }
--- src/test/java/com/austin/tddpractice/kata1/StringCalculatorTest.java
+++ src/test/java/com/austin/tddpractice/kata1/StringCalculatorTest.java
@@ -1,5 +1,27 @@
 package com.austin.tddpractice.kata1;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class StringCalculatorTest {
+
+    StringCalculator stringCalculator;
+
+    @Before
+    public void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void shouldSumZeroArguments() {
+        String arguments = "";
+
+        int sum = stringCalculator.Add(arguments);
+
+        assertThat(sum, is(0));
+    }
 
 }
